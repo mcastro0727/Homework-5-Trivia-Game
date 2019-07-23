93d521361e46ee6df3384aaff1ab9efbf4ba4f5a
Melissa Castro <melissa.anne.castro@gmail.com>
2019-07-22 22:20:45 -0400

    pseudocode in js

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Page with 'Start' button to start timer and game
+// Set timer for game
+// OPEN NEW PAGE
+// Multiple choice questions
+// Allow player one choice per question
+// 'Finish' at end of questions page
+// NEW PAGE
+// Tally Correct, Incorrect, and Uanswered Questions
+
