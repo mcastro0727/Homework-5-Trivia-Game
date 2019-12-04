cb7e599c680b64102d1729bf8a71926e8f4cdd57
Melissa Castro <melissa.anne.castro@gmail.com>
2019-12-03 22:53:13 -0500

    question rendering

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,31 +7,31 @@
 // NEW PAGE
 // Tally Correct, Incorrect, and Uanswered Questions
 
-$(document).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 
 
-// Lots of variables
-var gameTimer = 60;
-var timeRemaining;
+    // Lots of variables
+    var gameTimer = 60;
+    var timeRemaining;
 
-// To start timer and game
-$("#start").on("click", play);
-
-
-function play() {
-   clearInterval(timeRemaining);
-   timeRemaining = setInterval(countdown, 1000);
-   console.log(timeRemaining)
-}
+    // To start timer and game
+    $("#start").on("click", play);
 
 
-function countdown() {
-    gameTimer--;
-    $("#time-remaining").text(gameTimer)
-}
+    function play() {
+        clearInterval(timeRemaining);
+        timeRemaining = setInterval(countdown, 1000);
+        console.log(timeRemaining)
+    }
 
-play()
+
+    function countdown() {
+        gameTimer--;
+        $("#time-remaining").text(gameTimer)
+    }
+
+    play()
 
 
 });
@@ -40,41 +40,34 @@ play()
 
 
 
-function questionRenderer(objPassed){
-    var qDisplay = $("<p>")
-    qDisplay.text(objPassed.question)
+function questionRenderer() {
+    //set currentQuestion
+    this.currentQuestion = this.questions[this.currentIndex];
 
-    for (let i = 0; i < objPassed.length; i++) {
-        var ansDisplay = $("<input type='radio'>")
-        ansDisplay.attr("name", objPassed.name)
-        ansDisplay.attr("value", objPassed.ans[i])
+    //display question on page
+    $("#question").text(this.currentQuestion.question);
 
-        var textAns = $("<p>")
-        textAns.text(objPassed.ans[i])
-        
-        textAns.append(ansDisplay)
-
-
-
-
-        $("#choices").append(textAns)
-
-
-        $("#question").append(qDisplay)
-
-
-
-        $("#btn").on("click", function (){
-            var x = $("#choices").q1
-        })
+    //display answers on page
+    for (var i = 0; i < this.currentQuestion.answers.length; i++) {
+        var newAns = $("<a href='#' class='answer'>").text(this.currentQuestion.answers[i]);
+        $("#answers").append(newAns);
     }
-}
+
+    //countdown timer
+    $("#countdown").text(trivia.timer);
+    this.interval = setInterval(function () {
+        trivia.timer--;
+        $("#countdown").text(trivia.timer);
+        if (trivia.timer <= 0) {
+            trivia.progressGame();
+        }
+    }, 1000);
+};
 
 
 // Question Array
 var game = {
-    questions: [
-        {
+    questions: [{
             q1: "What animal can Harry speak to?",
             a1: ["Mouse", "Toad", "Snake", "Cat"],
             s1: 2,
@@ -114,7 +107,7 @@ var game = {
             a8: ["Ron Weasley", "Cho Chang", "Hermione Granger", "Ginny Weasley"],
             s8: 3,
         },
-            {
+        {
             q9: "Which of these is not one of Voldemort's horcruxes?",
             a9: ["Hufflepuff's Cup", "Slytherin's Locket", "Elder Wand", "Tom Riddle's Diary"],
             s9: 2,
@@ -124,6 +117,6 @@ var game = {
             a10: ["Albus Dumbledore", "Nicolas Flamel", "Voldemort", "James Potter"],
             s10: 1,
         },
-        
+
     ],
 }
